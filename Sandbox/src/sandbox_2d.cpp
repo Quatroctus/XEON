85ca8207c5ee523a2c09c593a1c7af57cdb4f749
James Taylor <kanadetachibona@gmail.com>
2020-09-13 20:40:19 -0700

    ImGui displays Renderer2D stats. There is also a rough Renderer2D stress test.

--- Sandbox/src/sandbox_2d.cpp
+++ Sandbox/src/sandbox_2d.cpp
@@ -10,6 +10,44 @@ Sandbox2D::Sandbox2D() : Layer("Sandbox2D"), cameraController(16.0F/9.0F, true) 
 
 Sandbox2D::~Sandbox2D() {}
 
+template<typename T, typename... Args>
+void emplace(T& t, Args&&... args) {
+	new(&t) T(std::forward<Args>(args)...);
+}
+
+void UpdateFrameData(XEON::Timestep delta) {
+	static float frames = 0.0F;
+	static float accumulated = 0.0F;
+	
+	constexpr float fps = 1.0F / 60.0F;
+	accumulated += delta;
+	frames++;
+	if (accumulated >= 1.0F) {
+		float framesPS = frames / accumulated * 1.0F;
+		XEON_INFO("FPS: {0}", framesPS);
+		frames -= framesPS;
+		accumulated -= 1.0F;
+	}
+}
+
+void BatchStress(XEON::Timestep delta, XEON::Ref<XEON::Texture>& texture) {
+	UpdateFrameData(delta);
+	constexpr uint64_t width = 500;
+	constexpr uint64_t size = width * width;
+	static bool init = true;
+	static glm::vec3 tileMap[size];
+	if (init) {
+	for (float x = 0; x < (float)width; x++)
+		for (float y = 0; y < (float)width; y++)
+			emplace(tileMap[(int)y * width + (int)x], x * 0.26F, y * 0.26F, 0.0F);
+	init = false;
+	}
+	constexpr glm::vec2 scale(0.25F);
+	for (uint32_t i = 0; i < size; i++) {
+		XEON::Renderer2D::DrawQuad(*(tileMap + i), scale, texture);
+	}
+}
+
 void Sandbox2D::onAttach() {
 	XEON_PROFILE_FN();
 	texture = XEON::Texture2D::Create("assets/textures/checkerboard.png");
@@ -20,55 +58,25 @@ void Sandbox2D::onDetach() {
 	texture = nullptr;
 }
 
-// Profiling data.
-int consistency = 0;
-int frames = 0;
-float frameTimes = 1.0F / 60.0F;
-
-
 void Sandbox2D::onUpdate(XEON::Timestep delta) {
-	// Profiling test.
-	
-	if (frames++ < 4) return;
-	if (delta > frameTimes) {
-		APP_TRACE("Timestep({0}) is greater than target({1}) at tile size ({2}, {3}) consistency({4})", delta, frameTimes, tileMapSize.x, tileMapSize.y, consistency);
-		if (consistency == 3) {
-			frameTimes *= 1.5F;
-			APP_TRACE("Consistency threshold met. Switching to {0} seconds.", frameTimes);
-			return;
-		} else {
-			consistency++;
-		}
-	} else consistency = 0;
-	
 	XEON_PROFILE_FN();
-
 	cameraController.onUpdate(delta);
-		
 	{
 		XEON_PROFILE_SCOPE("Renderer Prep");
 		XEON::RenderCommand::SetClearColor(glm::vec4{ 0.1F, 0.1F, 0.1F, 1.0F });
 		XEON::RenderCommand::Clear();
 	}
+
+	XEON::Renderer2D::ResetStats();
+	
 	{
 		XEON_PROFILE_SCOPE("Renderer Draw");
 		XEON::Renderer2D::BeginScene(cameraController.getCamera());
 		{
-			// Single Square Draw call test.
-			
-			for (float x = 0.0F; x < tileMapSize.x; x++) {
-				for (float y = 0.0F; y < tileMapSize.y; y++) {
-					XEON::Renderer2D::DrawQuad(glm::vec2{ 0.26F * x, 0.26F * y }, glm::vec2{ 0.25F, 0.25F }, squareColor);
-				}
-			}
-			
-			//XEON::Renderer2D::DrawQuad(glm::vec3{ 1.0F, 1.0F, -0.1 }, glm::vec2{ 3.0F, 3.0F }, texture, 10.0F);
-			//XEON::Renderer2D::DrawQuad(glm::vec2{ 0.0F, 0.0F }, glm::vec2{ 0.25F, 0.25F }, squareColor);
+			BatchStress(delta, texture);
 		}
-		XEON::Renderer2D::EndScene();
+		XEON::Renderer2D::EndScene();	
 	}
-	tileMapSize += growthRate * delta;
-
 }
 
 void Sandbox2D::onImGuiRender() {
@@ -76,6 +84,11 @@ void Sandbox2D::onImGuiRender() {
 	ImGui::Begin("Settings");
 	{
 		ImGui::ColorEdit4("Square Color", glm::value_ptr(squareColor));
+		auto stats = XEON::Renderer2D::GetStats();
+		ImGui::Text("Draw Calls: %d", stats.drawCalls);
+		ImGui::Text("Quad Count: %d", stats.quadCount);
+		ImGui::Text("\tVertex Count: %d", stats.getTotalVertexCount());
+		ImGui::Text("\tIndex Count: %d", stats.getTotalIndexCount());
 	}
 	ImGui::End();
 }
